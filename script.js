d9be82ce45ba0a6f7c46761ee088e3edebf77b6b
Shakhawat Hossain Rudra <sakhwt.hssain@gmail.com>
2025-04-09 22:31:31 +0600

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,19 @@ document.addEventListener("DOMContentLoaded", () => {
   img.style.borderRadius = "50%";
 
   const icons = document.querySelectorAll(".social-icons .icon");
+
   icons.forEach(icon => {
-    icon.addEventListener("click", () => {
+    icon.addEventListener("click", event => {
+      event.preventDefault(); // Stop immediate navigation
+
       icon.classList.add("clicked");
+
+      const link = icon.getAttribute("href");
+
+      // Delay navigation to show color change
+      setTimeout(() => {
+        window.open(link, "_blank");
+      }, 300);
     });
   });
 });
